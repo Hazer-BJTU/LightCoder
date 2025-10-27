551ab79f02cee5091028a3370eb602ae719f86b0
Hazer <22301092@bjtu.edu.cn>
2025-10-28 00:37:45 +0800

    2025/10/28-lcz

--- lcagent/src/lcagent/toolfunctions.py
+++ lcagent/src/lcagent/toolfunctions.py
@@ -2,7 +2,7 @@ import os
 import json
 
 from pathlib import Path
-from typing import Dict, Callable, Optional
+from typing import Dict, Callable, Optional, Literal, List
 from lcagent.langfetch import initialize_logger
 
 
@@ -11,7 +11,7 @@ LOGGER = initialize_logger(__file__)
 def fetch_project_file_structure_function(
     project_base_dir: str = '.',
     recursive: bool = True,
-    plain_text: bool = False,
+    return_form: Literal['dict', 'json', 'tree'] = 'tree',
     file_keep_rule: Optional[Callable[[str], bool]] = None,
     dir_keep_rule: Optional[Callable[[str], bool]] = None,
     category: Callable[[str], str] = lambda full_path : Path(full_path).suffix,
@@ -37,19 +37,37 @@ def fetch_project_file_structure_function(
         except Exception:
             raise
         return curr_output_dict
+    
+    tree_output: str = ''
+    tree_stack: List[Literal['not_last', 'last_one']] = []
+    def draw_tree(input_dict: Dict[str, Dict | str], target: str = '', layer: int = 0) -> None:
+        nonlocal tree_output, tree_stack
+        for i, flag in enumerate(tree_stack):
+            if i != len(tree_stack) - 1:
+                tree_output += '│   ' if flag == 'not_last' else '    '
+            else:
+                tree_output += '├── ' if flag == 'not_last' else '└── '
+        tree_output = tree_output + target + os.linesep
+        tree_stack.append('not_last')
+        for i, (key, value) in enumerate(input_dict.items()):
+            if i == len(input_dict.items()) - 1:
+                tree_stack[layer] = 'last_one'
+            draw_tree({} if isinstance(value, str) else value, key, layer + 1)
+        tree_stack.pop()
+
     try:
         result = fetch_inner(project_base_dir)
-        return result if not plain_text else json.dumps(result, indent=2)
     except Exception as e:
         LOGGER.warning(f'An error has occurred when traversing the project: {e}.')
         result = {}
-        return result if not plain_text else json.dumps(result, indent=2)
     
+    if return_form == 'dict':
+        return result
+    elif return_form == 'json':
+        return json.dumps(result, indent=2)
+    elif return_form == 'tree':
+        draw_tree(result)
+        return tree_output
 
 if __name__ == '__main__':
-    print(
-        fetch_project_file_structure_function(
-            plain_text=True,
-            dir_keep_rule=lambda dir: dir not in ['log', '__pycache__']
-        )
-    )
+    print(fetch_project_file_structure_function(return_form='tree'))
